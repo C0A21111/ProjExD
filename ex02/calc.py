f9dc1447bd8d6421a5b4a00ff3758e8e98a9c06b
kodama <c0a211110d@edu.teu.ac.jp>
2022-11-29 15:51:51 +0900

    calc02:x^yボタン

--- ex02/calc.py
+++ ex02/calc.py
@@ -2,7 +2,7 @@ import tkinter as tk
 # import tkinter.messagebox as tkm
 
 calc =tk.Tk()
-calc.geometry("300x500")
+calc.geometry("400x420")
 
 # テキスト入力欄
 entry=tk.Entry(calc,justify="right", width=10, font=("",40))
@@ -13,6 +13,7 @@ def btn_click(event):
     txt = btn["text"]
     if txt == "=":
         fml = entry.get()
+        fml = fml.replace("^","**")
         res = eval(fml)
         entry.delete(0,tk.END)
         entry.insert(tk.END,res)
@@ -21,20 +22,27 @@ def btn_click(event):
 
 # 数字ボタン
 for n in range(9,-1,-1):
-    btn = tk.Button(calc, text=f"{n}", width=4, height=2, font=("",30))
+    btn = tk.Button(calc, text=f"{n}", width=4, height=1, font=("",30))
     btn.bind("<1>", btn_click)
     if n==0:
         btn.grid(row=((9-n)//3)+2,column=((9-n)%3))
     else:
         btn.grid(row=((9-n)//3)+2,column=2-((9-n)%3))
 
+
+
+# [^]ボタン
+btn = tk.Button(calc, text=f"x^y", width=4, height=1, font=("",30))
+btn.bind("<1>", btn_click)
+btn.grid(row=1,column=0)
+
 # [+]ボタン
-btn = tk.Button(calc, text=f"+", width=4, height=2, font=("",30))
+btn = tk.Button(calc, text=f"+", width=4, height=1, font=("",30))
 btn.bind("<1>", btn_click)
 btn.grid(row=5,column=1)
 
 # [=]ボタン
-btn = tk.Button(calc, text=f"=", width=4, height=2, font=("",30))
+btn = tk.Button(calc, text=f"=", width=4, height=1, font=("",30))
 btn.bind("<1>", btn_click)
 btn.grid(row=5,column=2)
 
