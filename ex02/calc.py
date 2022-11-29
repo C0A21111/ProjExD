89a0da62ff16b152ce44bccaf3c7065bc6f38098
kodama <c0a211110d@edu.teu.ac.jp>
2022-11-29 15:02:53 +0900

    practice5-01:+=ボタン追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -16,4 +16,12 @@ for n in range(9,-1,-1):
     btn = tk.Button(calc, text=f"{n}", width=4, height=2, font=("",30))
     btn.bind("<1>", btn_click)
     btn.grid(row=((9-n)//3)+1,column=(9-n)%3)
+
+btn = tk.Button(calc, text=f"+", width=4, height=2, font=("",30))
+btn.bind("<1>", btn_click)
+btn.grid(row=4,column=1)
+
+btn = tk.Button(calc, text=f"=", width=4, height=2, font=("",30))
+btn.grid(row=4,column=2)
+
 calc.mainloop()
