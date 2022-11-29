58ed3cb4e77d8d9c002c2ea544c169afdbe9f0d8
kodama <c0a211110d@edu.teu.ac.jp>
2022-11-29 16:14:54 +0900

    calc03:[CE][C][-][×][÷][.]ボタン追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,19 +5,27 @@ calc =tk.Tk()
 calc.geometry("400x420")
 
 # テキスト入力欄
-entry=tk.Entry(calc,justify="right", width=10, font=("",40))
-entry.grid(columnspan=3)
+entry=tk.Entry(calc,justify="right", width=14, font=("",40))
+entry.grid(columnspan=4)
 
 def btn_click(event):
     btn = event.widget
     txt = btn["text"]
-    if txt == "=":
+    if txt == "CE":
+        entry.delete(0,tk.END)
+    elif txt == "C":
+        entry.delete(-2,tk.END)
+    elif txt == "=":
         fml = entry.get()
         fml = fml.replace("^","**")
+        fml = fml.replace("×","*")
+        fml = fml.replace("÷","/")
         res = eval(fml)
         entry.delete(0,tk.END)
         entry.insert(tk.END,res)
     else:
+        if txt == "x^y":
+            txt = "^"
         entry.insert(tk.END, txt)
 
 # 数字ボタン
@@ -25,13 +33,21 @@ for n in range(9,-1,-1):
     btn = tk.Button(calc, text=f"{n}", width=4, height=1, font=("",30))
     btn.bind("<1>", btn_click)
     if n==0:
-        btn.grid(row=((9-n)//3)+2,column=((9-n)%3))
+        btn.grid(row=((9-n)//3)+2,column=1)
     else:
         btn.grid(row=((9-n)//3)+2,column=2-((9-n)%3))
 
+# [C]ボタン
+btn = tk.Button(calc, text=f"C", width=4, height=1, font=("",30))
+btn.bind("<1>", btn_click)
+btn.grid(row=1,column=2)
 
+# [CE]ボタン
+btn = tk.Button(calc, text=f"CE", width=4, height=1, font=("",30))
+btn.bind("<1>", btn_click)
+btn.grid(row=1,column=1)
 
-# [^]ボタン
+# [x^y]ボタン
 btn = tk.Button(calc, text=f"x^y", width=4, height=1, font=("",30))
 btn.bind("<1>", btn_click)
 btn.grid(row=1,column=0)
@@ -39,11 +55,31 @@ btn.grid(row=1,column=0)
 # [+]ボタン
 btn = tk.Button(calc, text=f"+", width=4, height=1, font=("",30))
 btn.bind("<1>", btn_click)
-btn.grid(row=5,column=1)
+btn.grid(row=4,column=3)
+
+# [-]ボタン
+btn = tk.Button(calc, text=f"-", width=4, height=1, font=("",30))
+btn.bind("<1>", btn_click)
+btn.grid(row=3,column=3)
+
+# [×]ボタン
+btn = tk.Button(calc, text=f"×", width=4, height=1, font=("",30))
+btn.bind("<1>", btn_click)
+btn.grid(row=2,column=3)
+
+# [÷]ボタン
+btn = tk.Button(calc, text=f"÷", width=4, height=1, font=("",30))
+btn.bind("<1>", btn_click)
+btn.grid(row=1,column=3)
+
+# [.]ボタン
+btn = tk.Button(calc, text=f".", width=4, height=1, font=("",30))
+btn.bind("<1>", btn_click)
+btn.grid(row=5,column=2)
 
 # [=]ボタン
 btn = tk.Button(calc, text=f"=", width=4, height=1, font=("",30))
 btn.bind("<1>", btn_click)
-btn.grid(row=5,column=2)
+btn.grid(row=5,column=3)
 
 calc.mainloop()
