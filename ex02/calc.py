6ee3e3da10b5bf71954ad37d417bb3d337d74d96
kodama <c0a211110d@edu.teu.ac.jp>
2022-11-29 15:07:28 +0900

    practice6-01:文字の挿入

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import tkinter.messagebox as tkm
+# import tkinter.messagebox as tkm
 
 calc =tk.Tk()
 calc.geometry("300x500")
@@ -10,7 +10,11 @@ entry.grid(columnspan=3)
 def btn_click(event):
     btn = event.widget
     txt = btn["text"]
-    tkm.showinfo(txt, f"{txt}のボタンが押されました")
+    if txt == "=":
+        pass
+    else:
+        entry.insert(tk.END, txt)
+
 
 for n in range(9,-1,-1):
     btn = tk.Button(calc, text=f"{n}", width=4, height=2, font=("",30))
