5f208d8cadb47530664acf55992d11f213691ec5
kodama <c0a211110d@edu.teu.ac.jp>
2022-11-29 17:11:07 +0900

    calc07:おわり

--- ex02/calc.py
+++ ex02/calc.py
@@ -18,7 +18,7 @@ def btn_click(event):
     if txt == "CE":
         entry.delete(0,tk.END)
     elif txt == "C":
-        entry.delete(len(entry.get())-2,tk.END)
+        entry.delete(len(entry.get())-3,tk.END)
     elif txt == "←":
         entry.delete(len(entry.get())-1,tk.END)
     elif txt == "=":
@@ -38,10 +38,6 @@ def mk_btn(btn_txt,x,y):
     btn.bind("<1>", btn_click)
     btn.grid(row=x,column=y)
 
-# テキスト入力欄
-entry=tk.Entry(calc,justify="right", width=10, font=("",40))
-entry.grid(columnspan=3)
-
 # ボタンのリスト（配列）
 btn_txts = [["entry", None, None,"←"],
             ["x^y", "CE", "C", "÷"],
@@ -54,7 +50,9 @@ btn_txts = [["entry", None, None,"←"],
 for i in range(6):
     for j in range(4):
         if btn_txts[i][j]=="entry":
-            continue
+            # テキスト入力欄
+            entry=tk.Entry(calc,justify="right", width=10, font=("",40))
+            entry.grid(columnspan=3)
         elif not btn_txts[i][j]:
             continue
         else:
