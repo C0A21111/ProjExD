31965b76590449ef2c31e975dada7044d8b7363d
kodama <c0a211110d@edu.teu.ac.jp>
2022-11-29 14:28:31 +0900

    practice2-02:ボタン作成

--- ex02/calc.py
+++ ex02/calc.py
@@ -6,6 +6,6 @@ calc.geometry("300x500")
 r=3
 c=3
 for n in range(9,-1,-1):
-    btn = tk.Button(calc, text=f"{9-n}", width=4, height=2, font=("",30))
-    btn.grid(row=n//3,column=n%3)
+    btn = tk.Button(calc, text=f"{n}", width=4, height=2, font=("",30))
+    btn.grid(row=(9-n)//3,column=(9-n)%3)
 calc.mainloop()
