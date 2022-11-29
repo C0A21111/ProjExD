66331d97ef38bbc6211c01e23baa71b48ed0422d
kodama <c0a211110d@edu.teu.ac.jp>
2022-11-29 14:57:21 +0900

    practice4-01:テキスト欄

--- ex02/calc.py
+++ ex02/calc.py
@@ -4,6 +4,9 @@ import tkinter.messagebox as tkm
 calc =tk.Tk()
 calc.geometry("300x500")
 
+entry=tk.Entry(calc,justify="right", width=10, font=("",40))
+entry.grid(columnspan=3)
+
 def btn_click(event):
     btn = event.widget
     txt = btn["text"]
@@ -12,5 +15,5 @@ def btn_click(event):
 for n in range(9,-1,-1):
     btn = tk.Button(calc, text=f"{n}", width=4, height=2, font=("",30))
     btn.bind("<1>", btn_click)
-    btn.grid(row=(9-n)//3,column=(9-n)%3)
+    btn.grid(row=((9-n)//3)+1,column=(9-n)%3)
 calc.mainloop()
