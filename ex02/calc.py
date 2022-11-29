23dc5ac17cb31ab2473f3341c8964f04aaf4c2e8
kodama <c0a211110d@edu.teu.ac.jp>
2022-11-29 14:35:05 +0900

    practice3-01:bind

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,11 +1,16 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
 calc =tk.Tk()
 calc.geometry("300x500")
 
-r=3
-c=3
+def btn_click(event):
+    btn = event.widget
+    txt = btn["text"]
+    tkm.showinfo(txt, f"{txt}のボタンが押されました")
+
 for n in range(9,-1,-1):
     btn = tk.Button(calc, text=f"{n}", width=4, height=2, font=("",30))
+    btn.bind("<1>", btn_click)
     btn.grid(row=(9-n)//3,column=(9-n)%3)
 calc.mainloop()
