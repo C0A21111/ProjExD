6bd873d6a4893e87fcfee88e043ca5f202c730b8
kodama <c0a211110d@edu.teu.ac.jp>
2022-12-06 16:02:47 +0900

    maze_en01

--- ex03/maze.py
+++ ex03/maze.py
@@ -9,36 +9,33 @@ def key_up(event):
     global key
     key = ""
 
+def mk_img(kktn_num):
+    global image
+    image = tk.PhotoImage(file=f"fig/{kktn_num}.png")
+    canvas.create_image(mx*100+50,my*100+50,image=image,tag="kktn0")
+
 def main_proc():
-    global mx,my
-    if key=="Up":
-        if maze_lst[mx][my-1]==0:
-            my -= 1
-    if key=="Down":
-        if maze_lst[mx][my+1]==0:
-            my += 1
-    if key=="Left":
-        if maze_lst[mx-1][my]==0:
-            mx -= 1
-    if key=="Right":
-        if maze_lst[mx+1][my]==0:
-            mx += 1
+    global mx,my,image
+    if not key=="":
+        if key=="Up"    and maze_lst[mx][my-1]==0: my -= 1
+        if key=="Down"  and maze_lst[mx][my+1]==0: my += 1
+        if key=="Left"  and maze_lst[mx-1][my]==0: mx -= 1
+        if key=="Right" and maze_lst[mx+1][my]==0: mx += 1
+        mk_img(img_dic[key])
     canvas.coords("kktn0",mx*100+50,my*100+50)
     root.after(100,main_proc)
 
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+    img_dic = {"Up":6,"Down":3,"Left":5,"Right":2}
 
-    image = tk.PhotoImage(file="fig/0.png")
     canvas = tk.Canvas(width=1500,height=900,bg="#000000")
     mx = 1
     my = 1
-    cx = mx*100
-    cy = my*100
     maze_lst = mm.make_maze(15,9)
     mm.show_maze(canvas,maze_lst)
-    canvas.create_image(cx+50,cy+50,image=image,tag="kktn0")
+    mk_img(0)
     canvas.pack()
 
     key = ""
