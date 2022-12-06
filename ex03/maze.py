3d1057ee866f00d005abf570b4a53e5b29f631be
kodama <c0a211110d@edu.teu.ac.jp>
2022-12-06 14:16:41 +0900

    maze03

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,12 @@ if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
 
+    image = tk.PhotoImage(file="fig/0.png")
+
     canvas = tk.Canvas(width=1500,height=900,bg="#000000")
+    cx = 300
+    cy = 400
+    canvas.create_image(cx,cy,image=image,tag="kktn0")
+    canvas.pack()
 
     root.mainloop()
