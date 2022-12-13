eefd9425823550979b14288407a44111ac251cc4
kodama <c0a211110d@edu.teu.ac.jp>
2022-12-13 14:46:55 +0900

    prac05

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,11 +1,13 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock = pg.time.Clock()
 
     pg.display.set_caption("逃げろ！こうかとん") # タイトルバー
     scrn_sfc = pg.display.set_mode((1600,900)) # 1600x900のSurface
+    scrn_rct = scrn_sfc.get_rect()
 
     # 背景画像
     pgbg_sfc = pg.image.load("fig/pg_bg.jpg")
@@ -18,22 +20,27 @@ def main():
     tori_rct.center = 900, 400
     tori_sfc.blit(tori_sfc, tori_rct) # こうかとん画像blit
 
+    bomb_sfc = pg.Surface((20,20)) # 正方形の空Surface
+    bomb_sfc.set_colorkey((0,0,0))
+    pg.draw.circle(bomb_sfc, (255,0,0), (10,10),10)
+    bomb_rct = bomb_sfc.get_rect()
+    bomb_rct.centerx = random.randint(0,scrn_rct.width)
+    bomb_rct.centery = random.randint(0,scrn_rct.height)
+    scrn_sfc.blit(bomb_sfc, bomb_rct) # 爆弾画像blit
+
     while True:
         scrn_sfc.blit(pgbg_sfc, pgbg_rct) # 背景画像blit
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
         key_dct = pg.key.get_pressed()
-        if key_dct[pg.K_UP]:
-            tori_rct.centery -= 1
-        if key_dct[pg.K_DOWN]:
-            tori_rct.centery += 1
-        if key_dct[pg.K_RIGHT]:
-            tori_rct.centerx += 1
-        if key_dct[pg.K_LEFT]:
-            tori_rct.centerx -= 1
+        if key_dct[pg.K_UP]:    tori_rct.centery -= 1
+        if key_dct[pg.K_DOWN]:  tori_rct.centery += 1
+        if key_dct[pg.K_RIGHT]: tori_rct.centerx += 1
+        if key_dct[pg.K_LEFT]:  tori_rct.centerx -= 1
 
         scrn_sfc.blit(tori_sfc, tori_rct) # こうかとん画像blit
+        scrn_sfc.blit(bomb_sfc, bomb_rct) # 爆弾画像blit
         pg.display.update()
         clock.tick(1000)
 
