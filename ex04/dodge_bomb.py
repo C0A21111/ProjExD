3bc0cf9ac3a529111792037612239dc69074dc7d
kodama <c0a211110d@edu.teu.ac.jp>
2022-12-13 14:14:25 +0900

    prac02

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -7,15 +7,16 @@ def main():
     pg.display.set_caption("逃げろ！こうかとん") # タイトルバー
     scrn_sfc = pg.display.set_mode((1600,900)) # 1600x900のSurface
 
-    tori_sfc = pg.image.load("fig/pg_bg.jpg") # Surface
-    tori_sfc = pg.transform.rotozoom(tori_sfc, 0, 1.0)
-    tori_rct = tori_sfc.get_rect() # Rect
-    tori_rct.center = 800, 450
-    scrn_sfc.blit(tori_sfc, tori_rct) # blit
+    pgbg_sfc = pg.image.load("fig/pg_bg.jpg") # Surface
+    pgbg_rct = pgbg_sfc.get_rect() # Rect
+    pgbg_rct.center = 800, 450
 
-    pg.display.update()
-
-    clock.tick(0.5)
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+        scrn_sfc.blit(pgbg_sfc, pgbg_rct) # blit
+        pg.display.update()
+        clock.tick(1000)
 
 if __name__ == "__main__":
     pg.init()
